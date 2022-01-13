7acca212d7f9d4a77029aa4d63bd14ca778a7b74
Ivuuuu <Jasmine.cobb1611@gmail.com>
2022-01-13 09:49:19 -0500

    working

--- Games4Change_UnityProj/Assets/Scripts/RoomWFC.cs
+++ Games4Change_UnityProj/Assets/Scripts/RoomWFC.cs
@@ -6,6 +6,7 @@ public class RoomWFC : MonoBehaviour    // simple tiled WFC
 {
     // VARIABLES -----
     private static int roomDimension = 2;   // room tiles are square
+    private int totalTiles = 5;          // !!
 
     // create a 2D array of Dictionaries to simulate the room
     public Dictionary<string, GameObject>[,] roomMatrix = new Dictionary<string, GameObject>[roomDimension, roomDimension];
@@ -51,9 +52,28 @@ public class RoomWFC : MonoBehaviour    // simple tiled WFC
         finishedSuperposition = true;
     }
 
-    private void findEntropy()
+    private void findEntropy()  // find tile with lowest entropy (the lowest number of possible solutions)
     {
+        List<int> lowestEntropyCoords = new List<int>();  // save x y coords of cell with lowest entropy
+        int lowestEntropyAmount = totalTiles;
 
+        for (int x = 0; x < roomDimension; x++)
+        {
+            for (int y = 0; y < roomDimension; y++)
+            {
+                if (roomMatrix[x, y].Count < lowestEntropyAmount)     // num of items in dict < LEA
+                {
+                    lowestEntropyAmount = roomMatrix[x, y].Count;   // set lowEA to new lowest entropy
+                    lowestEntropyCoords.Clear();    // since there is a new lowest entropy clear the prev list coords
+                    lowestEntropyCoords.Add(x);
+                    lowestEntropyCoords.Add(y);     // add new x y coords to the list
+                }
+                else if (roomMatrix[x, y].Count == lowestEntropyAmount)     //an item with equal entropy to lowestEA
+                {
+
+                }
+            }
+        }
     }
     
 }
