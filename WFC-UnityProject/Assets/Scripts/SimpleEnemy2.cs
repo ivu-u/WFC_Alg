4e80bd855bc319c976a4611fe0f8ef0169246c0b
Ivuuuu <Jasmine.cobb1611@gmail.com>
2022-02-13 22:47:42 -0500

    :(

--- WFC-UnityProject/Assets/Scripts/SimpleEnemy2.cs
+++ WFC-UnityProject/Assets/Scripts/SimpleEnemy2.cs
@@ -1,17 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleEnemy : MonoBehaviour
+public class SimpleEnemy2 : MonoBehaviour
 {
     private float dirX;
     private float dirY;
     public float moveSpeed;
     private Rigidbody2D rb;
-    private bool facingRight = false;
-    private bool facingUp = false;
     private Vector3 localScale;
 
-    private bool isX = false;
-    private bool isY = false;
+    private bool facingRight = false;
+    private bool facingUp = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,63 +20,50 @@ public class SimpleEnemy : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         dirX = -1f;
         dirY = -1f;
+        rb.velocity = new Vector2(dirX * moveSpeed, 0 * moveSpeed);
+        dirX = 1f;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "wallTile")
         {
-            dirX *= -1f;
-            dirY *= -1f;
+            switchDirection();
+            Debug.Log("hit");
         }
 
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             collision.GetComponent<PlayerMovement>().Die();
         }
     }
 
-    void FixedUpdate()
-    {
-        /*if (isX)
-        {
-            rb.velocity = new Vector2(dirX * moveSpeed, moveSpeed);
-        }
-        else if (isY)
-        {
-            rb.velocity = new Vector2(moveSpeed, dirY * moveSpeed);
-        }*/
-    }
-
-    void LateUpdate()
-    {
-        CheckWhereToFace();
-    }
-
-    void CheckWhereToFace()
+    void switchDirection()
     {
         int randNum = Random.Range(1, 3);
 
         if (randNum == 1)
         {
-            isX = true;
-            isY = false;
+            Debug.Log("x1: " + dirX);
 
-            // check for X direction
             if (dirX > 0)
                 facingRight = true;
             else if (dirX < 0)
                 facingRight = false;
 
-            if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
-                localScale.x *= -1;
+            if (((facingRight) && (dirX > 0)) || ((!facingRight) && (dirX < 0)))
+            {
+                Debug.Log("changedirX");
+                dirX *= -1;
+            }
 
             rb.velocity = new Vector2(dirX * moveSpeed, 0 * moveSpeed);
+
+            Debug.Log("x: " + dirX);
         }
         else if (randNum == 2)
         {
-            isX = false;
-            isY = true;
+            Debug.Log("y1: " + dirY);
 
             // check for Y direction
             if (dirY > 0)
@@ -84,11 +71,15 @@ public class SimpleEnemy : MonoBehaviour
             else if (dirY < 0)
                 facingUp = false;
 
-            if (((facingUp) && (localScale.y < 0)) || ((!facingUp) && (localScale.y > 0)))
-                localScale.y *= -1;
+            if (((facingUp) && (dirY > 0)) || ((!facingUp) && (dirY < 0)))
+            {
+                Debug.Log("changedirY");
+                dirY *= -1;
+            }
 
             rb.velocity = new Vector2(0 * moveSpeed, dirY * moveSpeed);
+
+            Debug.Log("y: " + dirY);
         }
-        transform.localScale = localScale;
     }
 }
